6ead2069e39d1450e2cfc1faa42a181a81931d40
Noga Levy <nogalevy17@gmail.com>
2025-08-30 18:42:25 -0700

    I forgot most of it to be honest... but I DID make a gas sprite that will be used as an enemy (it's a bit complicated, but I got a plan)

--- Scripts/bleegHP.gd
+++ Scripts/bleegHP.gd
@@ -6,6 +6,9 @@ var total_health = null
 func _ready() -> void:
 	self.text = "[font_size=30]HP: {0}/{1}[/font_size]".format([health, total_health])
 
+func _process(delta: float) -> void:
+	self.text = "[font_size=30]HP: {0}/{1}[/font_size]".format([health, total_health])
+
 func _on_bleeg_send_health(HP: Variant, total_HP: Variant) -> void:
 	health = HP
 	total_health = total_HP
--- Scripts/enemy_bleeg.gd
+++ Scripts/enemy_bleeg.gd
@@ -1,13 +1,21 @@
 extends CharacterBody2D
 
+# Movement
 var bleeg_Vec = Vector2(0,0)
 var SPEED = 500
 var xdir = -1
 var ydir = -1
 
-func _process(delta: float) -> void:
-	bleeg_Vec[0] += 1
-	bleeg_Vec[1] += 1
+# Attack
+var inside_warble = []
+
+# Health
+var health = 13
+var total_health = 13
+signal send_health(HP, total_HP)
+
+func _physics_process(delta: float) -> void:
+	bleeg_Vec = Vector2(xdir, ydir)
 	
 	# normalize diagonal movement, scale by speed
 	if bleeg_Vec != Vector2.ZERO:
@@ -17,19 +25,52 @@ func _process(delta: float) -> void:
 	
 	velocity = SPEED * bleeg_Vec
 	move_and_slide()
+	
+	if inside_warble.size() > 0 and Input.is_action_just_pressed("attack"):
+		health -= 4
+	
+	send_health.emit(health, total_health)
 
 
-func _on_collision_area_entered(area: Area2D) -> void:
-	if area.is_in_group("border"):
-		if area.is_in_group("l-border"):
-			xdir = 1
-			ydir = -1
-		elif area.is_in_group("r-border"):
-			xdir = -1
-			ydir = 1
-		elif area.is_in_group("b-border"):
-			xdir = 1
-			ydir = 1
+func _process(delta: float) -> void:
+	if health <= 0:
+		self.queue_free()
+
+
+func attack():
+	await get_tree().create_timer(0.3).timeout
+	
+	if xdir == -1:
+		$Bleeg.play("left-attack")
+	else:
+		$Bleeg.play("right-attack")
+	
+	await get_tree().create_timer(0.2).timeout
+	
+	if inside_warble.size() > 0:
+		Global.crabHP -= 4
+	
+	if xdir == -1:
+		$Bleeg.play("left")
+	else:
+		$Bleeg.play("right")
+
+
+func _on_collision_body_entered(body: Node2D) -> void:
+	if body.is_in_group("border"):
+		xdir *= -1
+		ydir = randi_range(-1, 1)
+		
+		if xdir < 0:
+			$Bleeg.play("left")
 		else:
-			xdir = -1
-			ydir = -1
+			$Bleeg.play("right")
+	
+	if body.is_in_group("damager"):
+		inside_warble.append(body)
+		attack()
+
+
+func _on_collision_body_exited(body: Node2D) -> void:
+	if body.is_in_group("damager"):
+		inside_warble.erase(body)
