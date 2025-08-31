147ff23cd105d498359fbbe46cc3e5814ed4b3e0
Noga Levy <nogalevy17@gmail.com>
2025-08-31 15:05:10 -0700

    I fixed the gas--it turns out, the problem was that the scene wasn't in level 2, the Sprite2D of the gas was. THAT'S why nothing was happening. Whoops.

--- gas.gd
+++ gas.gd
@@ -1,7 +1,11 @@
 extends Area2D
 
 
+func _ready() -> void:
+	print("Gas is monitoring:", monitoring, " monitorable:", monitorable)
+
+
 func _on_body_entered(body: Node2D) -> void:
-	print("{body} is in the gas".format([body], "{body}"))
+	print("{body} is in the gas".format([body.name], "{body}"))
 	if body.is_in_group("Player"):
 		Global.crabHP -= 5
