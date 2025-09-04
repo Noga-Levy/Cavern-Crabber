e524f442f1766f1ab69b433a63aba08461b155ab
Noga Levy <nogalevy17@gmail.com>
2025-09-03 21:08:07 -0700

    Fixed a big with the box where if you pressed space without actually being anywhere near the box, it opens. Also added another lore image

--- health-box.gd
+++ health-box.gd
@@ -1,13 +1,26 @@
 extends Node2D
 
+var box_closed = true
+var colliding_with_box = []
+
 func _ready() -> void:
 	$Box.play("closed")
 	$"Health-potion".hide()
-	
-func _process(_delta: float) -> void:
-	if Input.is_key_pressed(KEY_SPACE):
+
+func _on_body_entered(body: Node2D) -> void:
+	colliding_with_box.append(body.name)
+
+func _on_body_exited(body: Node2D) -> void:
+	colliding_with_box.erase(body.name)
+
+
+func _process(delta: float) -> void:
+	if Input.is_key_pressed(KEY_SPACE) and box_closed and "Player" in colliding_with_box:
 		$Box.play("opened")
 		print("opened")
 		$"Health-potion".show()
+		
 		$Box/RichTextLabel.text = "You got a health potion! All HP has been restored"
 		Global.crabHP = Global.level * 5 + 5
+		
+		box_closed = false
