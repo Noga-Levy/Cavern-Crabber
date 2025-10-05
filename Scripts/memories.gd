7bbbded6dfceaadf4b8404a7907a24cafea8526a
Noga Levy <nogalevy17@gmail.com>
2025-10-04 17:43:49 -0700

    Combined the memories with the mushrooms (still have more to do--this is not a bug-less commit)

--- Scripts/memories.gd
+++ Scripts/memories.gd
@@ -5,6 +5,10 @@ var next_frame = false
 var special_frames = []
 signal down_pressed()
 
+signal pause()
+signal unpause()
+var touching_player
+
 var memories = []
 var memories_to_levels
 var memory_of_interest
@@ -12,12 +16,15 @@ var memory_of_interest
 
 
 func _ready() -> void:
+	$RichTextLabel.hide()
+	$AudioStreamPlayer.stop()
+	
 	for i in get_tree().get_nodes_in_group("memory"):
 		i.hide()
 		memories.append(i)
 		print("Hidden: ", i)
 	
-	memories_to_levels = {2 : memories[0], 3 : memories[1]}
+	memories_to_levels = {1 : memories[0], 2 : memories[1]}
 	
 	memory_of_interest = memories_to_levels[Global.level]
 	
@@ -26,11 +33,16 @@ func _ready() -> void:
 			frames.append(j)
 			j.hide()
 			print(j.name)
-	
-	frame_sequence()
 
 
 func frame_sequence():
+	emit_signal("pause")
+	
+	$Camera2D.make_current()
+	
+	$RichTextLabel.show()
+	$AudioStreamPlayer.play()
+	
 	memory_of_interest.show()
 	
 	for k in frames:
@@ -38,12 +50,33 @@ func frame_sequence():
 		print(k)
 		
 		await down_pressed
-		
-	if Global.level == 2:
-		get_tree().change_scene_to_file("res://Scenes/level_2.tscn")
-	else:
-		get_tree().change_scene_to_file("res://Scenes/level_3.tscn")
+	
+	$RichTextLabel.hide()
+	$AudioStreamPlayer.stop()
+	
+	for k in frames:
+		k.hide()
+	
+	emit_signal("unpause")
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		$Mushroom.play("highlight")
+		touching_player = true
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		$Mushroom.play("default")
+		touching_player = false
+
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_released("arrow-down"):
+	if Input.is_action_just_released("arrow-down") and touching_player:
 		emit_signal("down_pressed")
+		print("down is pressed")
+
+func _input(_event: InputEvent) -> void:
+	if touching_player and Input.is_action_just_released("space"):
+		frame_sequence()
--- Scripts/scroll.gd
+++ Scripts/scroll.gd
@@ -6,6 +6,7 @@ var stream_text_finished = false
 var all_info_shown = false
 var info
 
+
 func _ready() -> void:
 	Global.level += 1
 	
@@ -13,7 +14,7 @@ func _ready() -> void:
 	$"Ground-in-between-levels".modulate = level_colors[Global.level]
 	
 	$Level_complete.text = level_done
-	info = [$HP, $DMG, $SPEED, $New_Memory, $Next_Enemy]
+	info = [$HP, $DMG, $SPEED, $Next_Enemy]
 	
 	$HP.text = "[font_size=25][color=darkgreen]HP[/color][/font_size]\n[color=darkgreen]" + str(Global.level * 5) + " -> " + str(Global.level * 5 + 5) + "[/color]"
 	
@@ -24,11 +25,6 @@ func _ready() -> void:
 	$SPEED.text = "[font_size=25][color=orange]SPEED[/color][/font_size]\n[color=orange]" + str(500 + (Global.level - 2) * 100) + " -> " + str(500 + (Global.level - 1) * 100) +"[/color]"
 	Global.crab_SPEED = 500 + (Global.level - 1) * 100
 	Global.crab_modulate = "ffffff"
-	
-	if Global.level == 4:
-		$New_Memory.text = "[font_size=25][color=blue]No New Memories[/color][/font_size]"
-	else:
-		$New_Memory.text = "[font_size=25][color=blue]New memory unlocked[/color][/font_size]"
 	
 	var enemies = ["Warbles", "Bleegs", "Limmis"]
 	$Next_Enemy.text = "[font_size=25][color=black]NEXT UP: " + enemies[Global.level - 1] + "[/color][/font_size]\n[color=black]Press space to continue[/color]"
@@ -61,7 +57,7 @@ func _process(_delta: float) -> void:
 
 func _input(_event):
 	if Input.is_action_just_released("space"):
-		if Global.level == 4:
+		if Global.level == 3:
 			get_tree().change_scene_to_file("res://Scenes/level_3.tscn")
 		else:
-			get_tree().change_scene_to_file("res://Scenes/memories.tscn")
+			get_tree().change_scene_to_file("res://Scenes/level_2.tscn")
