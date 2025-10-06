9fcdfb7f99978d87f29c6b16fd012643dc7e006c
Noga Levy <nogalevy17@gmail.com>
2025-10-05 20:48:46 -0700

    Editted the mushroom-half-eaten-zoom image to include the rocks from the background. Also, there is still more to be added to the second memory--I forgot about a certain frame when I wrote the last commit.

--- Scripts/return-to-menu.gd
+++ Scripts/return-to-menu.gd
@@ -3,4 +3,6 @@ extends Area2D
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("space"):
+		$"Select-button".play()
+		await get_tree().create_timer(0.4).timeout
 		get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
