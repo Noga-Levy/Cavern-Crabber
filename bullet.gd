0202e6db519ce57b9a5868fcbd918d6b14c24dae
Noga Levy <nogalevy17@gmail.com>
2025-10-06 15:28:00 -0700

    Changed up the loading image, added comments to the bullet script, and did a couple changes/fixes to the memories and level 4.

--- bullet.gd
+++ bullet.gd
@@ -1,15 +1,19 @@
 extends CharacterBody2D
 
-var speed = 3600
+var speed = 3600  # It needs to be surprisingly fast
+
 
 func get_input():
-	velocity = transform.x * speed * (-1)
+	velocity = transform.x * speed * (-1)  # Negative goes to the left
 
+
+# Function to make the bullet actually move
 func _physics_process(_delta):
 	get_input()
 	move_and_slide()
 
 
+# Function to make the bullet deal damage and then disappear
 func _on_collision_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		Global.crabHP -= 3
