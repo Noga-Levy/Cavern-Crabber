3c829ccadfbdd43b0f5ce0d50e654750027e638f
Noga Levy <nogalevy17@gmail.com>
2025-10-13 13:20:37 -0700

    Making a ghost enemy; however, the movement and animations are, currently, very buggy

---
+++ Scripts/ghost.gd
@@ -0,0 +1,116 @@
+extends CharacterBody2D
+
+# Movement
+var ghost_Vec = Vector2(0,0)
+var SPEED = 1000
+var xdir = -1
+var ydir = -1
+
+# Attack
+var inside_ghost = []
+var dodge = false
+
+# Health
+var health = 5
+var total_health = health
+var damaged = Global.crab_damage
+signal send_health(HP, total_HP)
+
+func _ready() -> void:
+	$Ghost.play()
+
+func _physics_process(_delta: float) -> void:
+	
+	damaged = Global.crab_damage
+	
+	ghost_Vec = Vector2(xdir, ydir)
+	
+	# normalize diagonal movement, scale by speed
+	if ghost_Vec != Vector2.ZERO:
+		ghost_Vec = ghost_Vec.normalized()
+	else:
+		$Ghost.stop()
+	
+	velocity = SPEED * ghost_Vec
+	move_and_slide()
+	
+	if inside_ghost.size() > 0 and Input.is_action_just_pressed("attack"):
+		health -= damaged
+		damaged_animation()
+	
+	send_health.emit(health, total_health)
+
+
+func _process(_delta: float) -> void:
+	if health <= 0:
+		open_death_anim()
+		self.queue_free()
+		
+	if Input.is_action_pressed("dodge"):
+		start_dodge()
+
+
+func damaged_animation():
+	$damaged.show()
+	$damaged.play("default")
+	await get_tree().create_timer(0.5).timeout
+	$damaged.hide()
+
+
+func start_dodge():
+	dodge = true
+	await get_tree().create_timer(1.5).timeout  # duration of dodge window
+	dodge = false
+
+
+func attack():
+	await get_tree().create_timer(0.3).timeout
+	
+	# $hiss.play()
+	
+	if xdir == -1:
+		$Ghost.play("left-attack")
+	else:
+		$Ghost.play("right-attack")
+	
+	await get_tree().create_timer(0.2).timeout
+	
+	if inside_ghost.size() > 0 and not dodge:
+		Global.crabHP -= 4
+	
+	if xdir == -1:
+		$Ghost.play("left")
+	else:
+		$Ghost.play("right")
+
+
+func open_death_anim():
+	var death_scene = preload("res://Scenes/death_animation.tscn")
+	var death_instance = death_scene.instantiate()
+	
+	get_tree().root.get_node("level4").add_child(death_instance)
+	
+	death_instance.position = position
+	death_instance.modulate = "ffb1b2"
+
+
+func _on_collision_body_entered(body: Node2D) -> void:
+	if body.is_in_group("border") or body.is_in_group("Ghost"):
+		xdir *= -1
+		print(xdir)
+		ydir = randi_range(-1, 1)
+		$GhostBody.scale.x *= -1
+		$Collisions.scale.x *= -1
+		
+		if $GhostBody.scale.x < 0:
+			$Ghost.play("left")
+		else:
+			$Ghost.play("right")
+	
+	if body.is_in_group("damager"):
+		inside_ghost.append(body)
+		attack()
+
+func _on_collision_body_exited(body: Node2D) -> void:
+	if body.is_in_group("damager"):
+		inside_ghost.erase(body)
