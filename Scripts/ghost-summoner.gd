5855c686a6a6d59a3e81f4f47b1a2b6dbae94da3
Noga Levy <nogalevy17@gmail.com>
2025-10-14 11:06:33 -0700

    Created an "evil spirit" ghost (scene) and made the summoner only produce it.
    Since the sprites for ghosts 1 and 2 didn't really feel all that creative--and I didn't know how to make a dead adventurer *more* creative--I decided to scrap them and just make a go-to "evil spirit."

--- Scripts/ghost-summoner.gd
+++ Scripts/ghost-summoner.gd
@@ -1,22 +1,12 @@
 extends Node2D
 
 
-var ghost_list = [1, 2]
 var ghost_cd_starting_num = 5
 var ghost_cd = ghost_cd_starting_num
 
 
-func create_ghost1():
-	var ghost_scene = preload("res://Scenes/Ghosts/ghost_1.tscn")
-	var ghost_instance = ghost_scene.instantiate()
-	
-	get_tree().root.get_node("level4").add_child(ghost_instance)
-	
-	ghost_instance.position = self.position
-	
-
-func create_ghost2():
-	var ghost_scene = preload("res://Scenes/Ghosts/ghost_2.tscn")
+func create_evil_spirit():
+	var ghost_scene = preload("res://Scenes/Ghosts/evil_spirit.tscn")
 	var ghost_instance = ghost_scene.instantiate()
 	
 	get_tree().root.get_node("level4").add_child(ghost_instance)
@@ -28,13 +18,6 @@ func _process(delta: float) -> void:
 	ghost_cd -= delta
 	
 	if ghost_cd <= 0:
-		var chosen_ghost = ghost_list[randi() % ghost_list.size()]
-		if chosen_ghost == 1:
-			create_ghost1()
-		elif chosen_ghost == 2:
-			create_ghost2()
-		else:
-			print("Error: chosen_ghost " + str(chosen_ghost) + " does not exist")
-		
+		create_evil_spirit()
 		ghost_cd = ghost_cd_starting_num
 	
