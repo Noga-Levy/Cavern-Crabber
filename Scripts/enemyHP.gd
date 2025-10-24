84ec8488fd05f30a9d6849676b2b311f4f8c14e4
Noga Levy <nogalevy17@gmail.com>
2025-10-23 21:52:38 -0700

    Changed all enemy's (except for the warbles) HP scripts to the same one, renamed a couple things in the script so it's more universal, and deleted the now obselete scripts.
    Will try to deal with the warbles later.

--- Scripts/enemyHP.gd
+++ Scripts/enemyHP.gd
@@ -4,9 +4,12 @@ var health = null
 var total_health = null
 
 func _ready() -> void:
-	self.text = "HP: {0}/{1}".format([health, total_health])
+	self.text = "[font_size=30]HP: {0}/{1}[/font_size]".format([health, total_health])
 
-func _on_enemy_warble_send_health(HP: Variant, total_HP: Variant) -> void:
+func _process(_delta: float) -> void:
+	self.text = "[font_size=30]HP: {0}/{1}[/font_size]".format([health, total_health])
+
+func _on_send_health(HP: Variant, total_HP: Variant) -> void:
 	health = HP
 	total_health = total_HP
-	self.text = "HP: {0}/{1}".format([health, total_health])
+	self.text = "[font_size=30]HP: {0}/{1}[/font_size]".format([health, total_health])
