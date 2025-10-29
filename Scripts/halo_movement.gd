69d16041905cc0d8be3d606089189c867369be9b
Noga Levy <nogalevy17@gmail.com>
2025-10-28 17:07:46 -0700

    Fixed an issue with the level 5 audio, added another thing to credits and got the game demo-ready.

--- Scripts/halo_movement.gd
+++ Scripts/halo_movement.gd
@@ -2,8 +2,8 @@ extends AudioStreamPlayer
 
 
 func _on_pearl_unpause() -> void:
-	$"level5-bg-music".play(Global.music_pos)
+	self.play(Global.music_pos)
 
 
-func _on_pearl_pause() -> void:
-	Global.music_pos = $"level5-bg-music".get_playback_position()
+func _process(_delta: float) -> void:
+	Global.music_pos = self.get_playback_position()
